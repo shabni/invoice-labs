08ad056ad6c8ea21773f1bb4b10c060d8cdae4ad
shabni <ahsanahmad@gmail.com>
2022-06-16 16:37:28 +0500

    selected tab functionality implemented

--- src/app/pages/side-bar/side-bar.component.ts
+++ src/app/pages/side-bar/side-bar.component.ts
@@ -8,7 +8,9 @@ import { FormControl } from '@angular/forms';
 })
 export class SideBarComponent {
 
-  dataList = [ {title:'Estimate', rout:'extimates'},{title:'Clients', rout:'clients'},{title:'Items', rout:'items'},{title:'Reports', rout:'reports'},
+  selectedView = 'Items'
+
+  dataList = [{title:'Invoice', rout:'invoices'}, {title:'Estimate', rout:'extimates'},{title:'Clients', rout:'clients'},{title:'Items', rout:'items'},{title:'Reports', rout:'reports'},
   {title:'business Details', rout:'business-details'},{title:'Region', rout:'regions'},{title:'Choose Template', rout:'templates'},{title:'Upgrade Subscription', rout:'subscription'},
   {title:'Settings', rout:'settings'},{title:'Contact Us', rout:'contact'},
   ]
